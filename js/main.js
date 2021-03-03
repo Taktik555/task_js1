73314b3ba5ad60069e4ad2e95632895167648308
Taktik555 <79668851+Taktik555@users.noreply.github.com>
2021-03-03 22:53:10 +0300

    Updated closing popup

--- js/main.js
+++ js/main.js
@@ -4,15 +4,23 @@ var popup_close = document.getElementsByClassName('popup_close');
 header_btn[0].addEventListener('click', popup_engineer_btn);
 
 for (var i=0; i<popup_close.length; i++){
-	popup_close[i].addEventListener('click', popup_close_btn);
+	popup_close[i].addEventListener('click', function(){popup_close_btn(this.className)});
 }
 function popup_engineer_btn() {
 	popup_engineer[0].style.display = 'flex';
+	popup_engineer[0].addEventListener('click', function(){popup_close_btn(this.className)});
 }
 
-function popup_close_btn() {
-	popup_engineer[0].style.display = 'none';
-	popup[0].style.display = 'none';
+function popup_close_btn(popup_name) {
+	var target = event.target;
+	var close_but = null;
+	if(target.closest('button')){
+		close_but = target.closest('button').className;
+	}
+	if ((target.className == popup_name) || (close_but == 'popup_close')){
+		popup_engineer[0].style.display = 'none';
+		popup[0].style.display = 'none';
+	}
 }
 
 
@@ -23,4 +31,5 @@ for (var i=0; i<phone_link.length; i++){
 }
 function popup_btn(){
 	popup[0].style.display = 'flex';
+	popup[0].addEventListener('click', function(){popup_close_btn(this.className)});
 }
